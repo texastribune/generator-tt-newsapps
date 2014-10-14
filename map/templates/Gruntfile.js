9f19f3cd5697d1889225ca04ebc84313f62f03fd
Marcos Vanetta <marcosvanetta@gmail.com>
2014-10-14 11:17:56 -0500

    JSHint ignores templates.js and pymload.js

--- map/templates/Gruntfile.js
+++ map/templates/Gruntfile.js
@@ -152,6 +152,10 @@ module.exports = function (grunt) {
         'Gruntfile.js',
         '<%%= config.app %>/scripts/{,*/}*.js',
         '!<%%= config.app %>/scripts/vendor/*'
+      ],
+      ignores: [
+        '<%%= config.app %>/scripts/templates.js',
+        '<%%= config.app %>/scripts/pymload.js'
       ]
     },
 
