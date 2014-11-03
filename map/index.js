9c382172b832062d97c62773e69bb1743c38efc5
Ryan Murphy <github@accounts.rdmurphy.org>
2014-11-03 14:51:27 -0600

    jshintignore no longer needed

--- map/index.js
+++ map/index.js
@@ -63,10 +63,6 @@ module.exports = yeoman.generators.Base.extend({
     this.copy('jshintrc', '.jshintrc');
   },
 
-  jshintignore: function() {
-    this.copy('jshintignore', '.jshintignore');
-  }
-
   editorconfig: function() {
     this.copy('editorconfig', '.editorconfig');
   },
